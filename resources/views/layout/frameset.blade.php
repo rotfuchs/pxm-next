a5ac2f4d1f4bff2ca5bcd01378846169a0ddc242
Markus Voss <mv@filmvisionaere.com>
2017-11-01 18:24:18 +0100

    added favicon

--- resources/views/layout/frameset.blade.php
+++ resources/views/layout/frameset.blade.php
@@ -6,6 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#a51425">
+    <meta name="apple-mobile-web-app-title" content="TeleMassaker">
+    <meta name="application-name" content="TeleMassaker">
+    <meta name="theme-color" content="#ff0000">
+
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <script src="{{ mix('/js/app.js') }}"></script>
     <script src="{{ mix('/js/all.js') }}"></script>
