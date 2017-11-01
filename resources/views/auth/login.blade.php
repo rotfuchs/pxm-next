2debdabe8151d60f8bfbbc43d291b9510630ef7d
Markus Voss <mv@filmvisionaere.com>
2017-11-01 19:58:28 +0100

    added login form

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,32 @@
+<div class="loginContainer">
+    <div class="defaultHeader">
+        <span class="icon-arrow-right"></span> Login
+    </div>
+
+    <div class="formContainer">
+        <form>
+            <div class="formElements">
+                <label>
+                    Username:
+                    <input type="text" name="username" value="" placeholder="" />
+                </label>
+                <label>
+                    Passwort:
+                    <input type="password" name="password" value="" />
+                </label>
+
+                <input type="submit" value="Login" />
+            </div>
+            <div class="formInfo">
+                Erst nach dem Login sind alle<br />
+                Funktionen des Forums verfügbar.
+            </div>
+        </form>
+    </div>
+
+    <div class="loginFooter">
+        Vor dem Erstellen neuer Beiträge bitte die Regeln und Hinweise des <a href="/faq">FORUM-FAQs</a> beachten!
+        Nach dem Login können im <span class="highlight">SETUP</span> die Daten des User-Profils, das Passwort und diverse Einstellungen
+        des Forums geändert werden. Viel Spaß!
+    </div>
+</div>
--- resources/views/board/index.blade.php
+++ resources/views/board/index.blade.php
@@ -3,23 +3,30 @@
 @section('title', trans('board.title'))
 
 @section('content')
-    <div class="defaultHeader"></div>
-    <table class="default" id="boardTable">
-        <thead>
-        <tr>
-            <th>Rubrik</th>
-            <th>Letzter Beitrag</th>
-            <th>Moderator(en)</th>
-        </tr>
-        </thead>
+    @include('auth.login')
 
-        <tbody>
+    <div class="boardIndex">
+        <div class="defaultHeader">
+            <span class="icon-arrow-right"></span> Rubrik-Index
+        </div>
+        <table class="default" id="boardTable">
+            <thead>
+            <tr>
+                <th>Rubrik</th>
+                <th>Letzter Beitrag</th>
+                <th>Moderator(en)</th>
+            </tr>
+            </thead>
+
+            <tbody>
             @foreach($boards as $board)
                 <?php /** @var \App\Services\Board\View\BoardIndexTableRowView $board */ ?>
                 {!! $board !!}
             @endforeach
-        </tbody>
-    </table>
+            </tbody>
+        </table>
+    </div>
+
 
     <script>
         window.onload = function () {
