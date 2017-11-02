01c4fe34e9b4849d30645cffee5b75133045c641
Markus Voss <mv@filmvisionaere.com>
2017-11-02 21:56:44 +0100

    improved frame usability

--- resources/assets/js/frameset/Frameset.js
+++ resources/assets/js/frameset/Frameset.js
@@ -1,37 +1,87 @@
 export function initClassicFrameset()
 {
-    interact('.threadViewContainer')
+    // interact('.threadViewContainer')
+    //     .resizable({
+    //         edges: { bottom: true }
+    //     })
+    //     .on('resizemove', function (event) {
+    //         let target = event.target;
+    //         let x = (parseFloat(target.getAttribute('data-x')) || 0);
+    //         let y = (parseFloat(target.getAttribute('data-y')) || 0);
+    //         let postContainerEl = document.querySelectorAll('.postContainer');
+    //         let postTreeContainerEl = document.querySelectorAll('.postTreeContainer')[0];
+    //         let threadViewHeight = calculateElementHeightPercent(event.rect.height);
+    //
+    //         let postHeightPxNum = parseInt(getComputedStyle(postContainerEl[0])['height'].replace('px', ''));
+    //         let postHeight = calculateElementHeightPercent(postHeightPxNum);
+    //         let postTreeHeight = 100 - threadViewHeight - postHeight;
+    //
+    //
+    //
+    //         if(postTreeHeight<1 || threadViewHeight<1)
+    //             return;
+    //
+    //         // update the element's style
+    //         // target.style.width  = event.rect.width + 'px';
+    //         // target.style.height = threadViewHeight + 'vh';
+    //         $(target)
+    //             .css('height', threadViewHeight + 'vh')
+    //             .css('min-height', threadViewHeight + 'vh');
+    //
+    //         // postTreeContainerEl.style.height = postTreeHeight + 'vh';
+    //         $(postTreeContainerEl)
+    //             .css('height', postTreeHeight + 'vh')
+    //             .css('min-height', postTreeHeight + 'vh');
+    //
+    //         // $('.threadViewContainer').find('.wrapper').css('height', threadViewHeight + 'vh');
+    //         // $('.postTreeContainer').find('.wrapper').css('min-height', postTreeHeight + 'vh');
+    //         // $('.postContainer').find('.wrapper').css('min-height', postHeight + 'vh');
+    //
+    //         // translate when resizing from top or left edges
+    //         x += event.deltaRect.left;
+    //         y += event.deltaRect.top;
+    //
+    //         // target.style.webkitTransform = target.style.transform =
+    //         //     'translate(' + x + 'px,' + y + 'px)';
+    //
+    //
+    //         target.setAttribute('data-x', x);
+    //         target.setAttribute('data-y', y);
+    //         // target.textContent = Math.round(event.rect.width) + '×' + Math.round(event.rect.height);
+    //     });
+
+    interact('.postTreeContainer')
         .resizable({
-            edges: { bottom: true }
+            edges: { top: '.defaultHeader' }
         })
         .on('resizemove', function (event) {
             let target = event.target;
             let x = (parseFloat(target.getAttribute('data-x')) || 0);
             let y = (parseFloat(target.getAttribute('data-y')) || 0);
             let postContainerEl = document.querySelectorAll('.postContainer');
-            let postTreeContainerEl = document.querySelectorAll('.postTreeContainer')[0];
-            let threadViewHeight = calculateElementHeightPercent(event.rect.height);
+            let threadListContainerEl = document.querySelectorAll('.threadViewContainer')[0];
+            let postTreeViewHeight = calculateElementHeightPercent(event.rect.height);
 
             let postHeightPxNum = parseInt(getComputedStyle(postContainerEl[0])['height'].replace('px', ''));
             let postHeight = calculateElementHeightPercent(postHeightPxNum);
-            let postTreeHeight = 100 - threadViewHeight - postHeight;
+            let threadListHeight = 100 - postTreeViewHeight - postHeight;
 
 
 
-            if(postTreeHeight<1 || threadViewHeight<1)
+            if(threadListHeight<1 || postTreeViewHeight<1)
                 return;
 
             // update the element's style
             // target.style.width  = event.rect.width + 'px';
             // target.style.height = threadViewHeight + 'vh';
             $(target)
-                .css('height', threadViewHeight + 'vh')
-                .css('min-height', threadViewHeight + 'vh');
+                .css('height', postTreeViewHeight + 'vh')
+                .css('min-height', postTreeViewHeight + 'vh');
 
             // postTreeContainerEl.style.height = postTreeHeight + 'vh';
-            $(postTreeContainerEl)
-                .css('height', postTreeHeight + 'vh')
-                .css('min-height', postTreeHeight + 'vh');
+            $(threadListContainerEl)
+                .css('height', threadListHeight + 'vh')
+                .css('min-height', threadListHeight + 'vh');
 
             // $('.threadViewContainer').find('.wrapper').css('height', threadViewHeight + 'vh');
             // $('.postTreeContainer').find('.wrapper').css('min-height', postTreeHeight + 'vh');
@@ -52,7 +102,7 @@ export function initClassicFrameset()
 
     interact('.postContainer')
         .resizable({
-            edges: { top: true }
+            edges: { top: '.defaultHeader' }
         })
         .on('resizemove', function (event) {
             let target = event.target;
@@ -110,5 +160,4 @@ export function initClassicFrameset()
 
 export function initAdvancedFrameset()
 {
-    console.log('bla');
 }
