e988cb544a6206ba640ead6c754f83d7f220a70e
Markus Voss <mv@filmvisionaere.com>
2017-11-03 20:00:43 +0100

    added redirect route

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::redirect('/', '/boards');
 
 
 Route::get('/test', 'TestController@test');
